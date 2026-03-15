8d9f309c5276e3d49be968b792af0841f4ba5b1d
Kousujo <ungnguyentienthanh9.1nvx@gmail.com>
2026-03-15 09:52:24 +0700

    Share Bai Tap

---
+++ Bai_tap_chuong_8/Bai2.java
@@ -0,0 +1,67 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Bai2 extends JFrame implements ActionListener {
+    private JTextField txtNhapchuoi;
+    private JLabel lblKetqua;
+    private JButton btnUpper, btnLower;
+    private JPanel mainPanel, buttonPanel;
+
+    public Bai2() {
+        setTitle("Form đổi chữ hoa - chữ thường");    
+        setSize(400, 250);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        Font font = new Font("Arial", Font.PLAIN, 18);
+
+        mainPanel = new JPanel();
+        mainPanel.setLayout(new BorderLayout(10, 10));
+        mainPanel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+
+        buttonPanel = new JPanel();
+        buttonPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 20, 0));
+        buttonPanel.setBorder(BorderFactory.createEmptyBorder(20, 0, 20, 0));
+
+        txtNhapchuoi = new JTextField("Nhập chuỗi tại đây");
+        txtNhapchuoi.setFont(font);
+
+        lblKetqua = new JLabel("Kết quả sẽ hiển thị ở đây");
+        lblKetqua.setHorizontalAlignment(SwingConstants.CENTER);
+        lblKetqua.setBackground(Color.LIGHT_GRAY);
+        lblKetqua.setBorder(BorderFactory.createLineBorder(Color.GRAY));
+        lblKetqua.setFont(font);
+
+        btnUpper = new JButton("Chữ hoa");
+        btnUpper.setFont(new Font("Arial", Font.BOLD, 18));
+        btnLower = new JButton("Chữ thường");
+        btnLower.setFont(new Font("Arial", Font.BOLD, 18));
+
+        buttonPanel.add(btnUpper, BorderLayout.WEST);
+        buttonPanel.add(btnLower, BorderLayout.EAST);
+
+        mainPanel.add(lblKetqua, BorderLayout.CENTER);
+        mainPanel.add(txtNhapchuoi, BorderLayout.NORTH);
+        mainPanel.add(buttonPanel, BorderLayout.SOUTH);
+        this.add(mainPanel);
+
+        btnUpper.addActionListener(this);
+        btnLower.addActionListener(this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String input = txtNhapchuoi.getText();
+        if (e.getSource() == btnUpper) {
+            lblKetqua.setText("Kết quả: " + input.toUpperCase());
+        } else if (e.getSource() == btnLower) {
+            lblKetqua.setText("Kết quả: " + input.toLowerCase());
+        }
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            new Bai2().setVisible(true);
+        });
+    }
+}
---
+++ Bai_tap_chuong_8/Bai3.java
@@ -0,0 +1,81 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.border.Border;
+
+public class Bai3 extends JFrame implements ActionListener {
+    private JPanel mainPanel, checkboxPanel, buttonPanel;
+    private JLabel lblHinhthuc;
+    private JCheckBox chxOn, chxOff;
+    private JButton btnXacnhan;
+
+    public Bai3() {
+        setTitle("Form chọn hình thức học");    
+        setSize(400, 250);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        Font font = new Font("Arial", Font.PLAIN, 18);
+
+        mainPanel = new JPanel();
+        mainPanel.setLayout(new BorderLayout(10, 10));
+        mainPanel.setBorder(BorderFactory.createEmptyBorder(20, 30, 20, 20));
+
+        checkboxPanel = new JPanel();
+        checkboxPanel.setLayout(new GridLayout(2, 1, 10, 15));
+        Border padding = BorderFactory.createEmptyBorder(20, 20, 10, 20);
+        Border line = BorderFactory.createLineBorder(Color.GRAY);
+        checkboxPanel.setBorder(BorderFactory.createCompoundBorder(line, padding));
+
+        buttonPanel = new JPanel();
+        buttonPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+
+        lblHinhthuc = new JLabel("Chọn hình thức học");
+        lblHinhthuc.setFont(new Font("Arial", Font.BOLD, 24));
+        lblHinhthuc.setHorizontalAlignment(SwingConstants.CENTER);
+
+        chxOn = new JCheckBox("ONLINE");
+        chxOff = new JCheckBox("OFFLINE");
+        chxOn.setFont(font);
+        chxOff.setFont(font);
+        checkboxPanel.add(chxOn);
+        checkboxPanel.add(chxOff);
+        
+        btnXacnhan = new JButton("Xác Nhận");
+        btnXacnhan.setFont(new Font("Arial", Font.BOLD, 18));
+        buttonPanel.add(btnXacnhan, BorderLayout.CENTER);
+
+        mainPanel.add(lblHinhthuc, BorderLayout.NORTH);
+        mainPanel.add(checkboxPanel, BorderLayout.CENTER);
+        mainPanel.add(buttonPanel, BorderLayout.SOUTH);
+        this.add(mainPanel);
+
+        btnXacnhan.addActionListener(this);
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == btnXacnhan) {
+            if (!chxOn.isSelected() && !chxOff.isSelected()) {
+                JOptionPane.showMessageDialog(this, "Vui lòng chọn ít nhất 1 hình thức học!");
+                return; 
+            }
+
+            String result = "Hình Thức Học: ";
+            if (chxOn.isSelected() && chxOff.isSelected()) {
+                result += "ONLINE và OFFLINE";
+            } else if (chxOn.isSelected()) {
+                result += "ONLINE";
+            } else {
+                result += "OFFLINE";
+            }
+
+            JOptionPane.showMessageDialog(this, result);
+        }
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            new Bai3().setVisible(true);
+        });
+    }
+}
